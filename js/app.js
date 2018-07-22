e901cec5382a16f23e365e4d79433475b3a8a26a
Casey Fawcett <caseyfawcett@gmail.com>
2018-07-22 13:53:52 -0700

    Assignment 6 completed

--- js/app.js
+++ js/app.js
@@ -1,44 +1,58 @@
 function onReady() {
   let toDoId = 0;
-  const TO_DOS = [];
+  let toDos = [];
   const ADD_TO_DO_FORM = document.getElementById('addToDoForm');
 
   function createNewToDo() {
     const NEW_TO_DO_TEXT = document.getElementById('newToDoText');
     if (!NEW_TO_DO_TEXT.value) { return; }
 
-    TO_DOS.push({
+    toDos.push({
       title: NEW_TO_DO_TEXT.value,
-      complete: false,
-      id: toDoId.value
+      complete:false,
+      id: toDoId
     });
 
     toDoId++;
-    console.log(TO_DOS.id);
 
     NEW_TO_DO_TEXT.value = '';
-
     renderTheUi();
-
   }
 
   function renderTheUi() {
     const TO_DO_LIST = document.getElementById('toDoList');
 
-    TO_DO_LIST.textContent ='';
+    TO_DO_LIST.textContent = '';
 
-    TO_DOS.forEach(function(toDo) {
-
+    toDos.forEach(function(toDo) {
       const NEW_LI = document.createElement('li');
       const CHECKBOX = document.createElement('input');
       CHECKBOX.type = "checkbox";
+
+      const DELETE_BTN = document.createElement('button');
+      DELETE_BTN.textContent = 'x';
+
+      //MDL Styles
+      DELETE_BTN.className = 'mdl-button mdl-js-button';
+      NEW_LI.className = 'mdl-list__item';
+
+      DELETE_BTN.addEventListener('click', event => {
+        toDos = toDos.filter(function(item) {
+          return item.id !== toDo.id;
+        })
+
+        renderTheUi();
+
+      })
+
 
       NEW_LI.textContent = toDo.title;
 
       TO_DO_LIST.appendChild(NEW_LI);
       NEW_LI.appendChild(CHECKBOX);
-    });
+      NEW_LI.appendChild(DELETE_BTN);
 
+    });
   }
 
   ADD_TO_DO_FORM.addEventListener('submit', event => {
@@ -46,6 +60,9 @@ function onReady() {
     createNewToDo();
   });
 
+  //Upgrade all the MDL elements
+  componentHandler.upgradeDom();
+  
   renderTheUi();
 }
 
